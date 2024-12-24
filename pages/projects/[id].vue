00fb1633946c689a8573a3f1e9f96839e405b8e0
Figma beebee <nawgloriawinnyunt_n@cmu.ac.th>
2024-12-24 15:17:37 +0700

    Route Parameters

---
+++ pages/projects/[id].vue
@@ -0,0 +1,18 @@
+ <!-- Route parameters : /products/:id eg- /products/1 -->
+
+<script setup lang="ts">
+   const {id} = useRoute().params
+</script>
+
+<template>
+    <div>
+        <p>Product details for {{ id }}</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint nam saepe amet sequi illo.</p>
+    </div>
+</template>
+
+
+
+<style scoped>
+
+</style>
