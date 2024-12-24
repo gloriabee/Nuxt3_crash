cb0346e3fec1eaf5420161f6c7bd8bb065db4c12
Figma beebee <nawgloriawinnyunt_n@cmu.ac.th>
2024-12-24 16:46:18 +0700

    add style with tailwind in assets folder

--- layouts/default.vue
+++ layouts/default.vue
@@ -13,7 +13,7 @@
                     <li><NuxtLink to="/">Home</NuxtLink></li>
                     <li><NuxtLink to="about">About</NuxtLink></li>
                     <li><NuxtLink to="projects">Projects</NuxtLink></li>
-                    <li><NuxtLink to="contact">Contact</NuxtLink></li>
+                    <li><NuxtLink to="contact" class="btn">Contact</NuxtLink></li>
                 </ul>
             </nav>
         </header>
